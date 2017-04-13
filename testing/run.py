084205ec665312eb21ffe20e416dea6fac6bc672
Jianqin Wang <jianqinwang94@gmail.com>
2017-04-13 13:16:48 -0700

    testing file for server

---
+++ testing/run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse, Message
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """
+    Respond to incoming calls with a simple text message
+    """
+
+    resp = MessagingResponse()
+    msg = Message().body("Hello, Mobile Monkey").media("https://demo.twilio.com/owl.png")
+    resp.append(msg)
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
