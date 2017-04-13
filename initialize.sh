25d4a2bdd8900c8119feeb92733ef8603046cd2e
Jianqin Wang <jianqinwang94@gmail.com>
2017-04-13 15:05:11 -0700

    script to setup new EC2 instance

---
+++ initialize.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install python3
+sudo apt-get -y install python3-dev
+sudo apt-get -y install python3-pip
+sudo pip3 install flask
+sudo pip3 install twilio
+sudo pip3 install uwsgi
+sudo apt-get -y install nginx
+sudo service nginx start
